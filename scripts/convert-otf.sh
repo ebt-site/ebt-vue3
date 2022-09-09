4d55078448c590bfd8aa8c58b68238313f2f1ad2
Karl Lew <karl@firepick.org>
2022-09-09 07:33:46 -0700

    v1.7.0 += SourceSansPro

--- scripts/convert-otf.sh
+++ scripts/convert-otf.sh
@@ -8,15 +8,19 @@ echo -e "$SCRIPT: BEGIN `date`"
 
 set -e
 
+HELP="$SCRIPT: EXPECTED => $SCRIPT input.otf /path/to/output/"
+
 SOURCE=$1
+if [ "$SOURCE" == "" ]; then
+  echo $HELP
+  exit 1
+fi
+
 DESTINATION=$2
 if [ "$DESTINATION" == "" ]; then
   DESTINATION=public/fonts
 fi
-FILENAME=$(basename "${SOURCE%.otf}")
-FAMILY=`basename -s .otf $SOURCE | sed -e "s/-.*//"`
 
-HELP="$SCRIPT: EXPECTED => $SCRIPT input.otf /path/to/output/"
 # we omit mime check since we use this script only locally and assume it's otf
 if [ -e $SOURCE ]; then
   echo -e "$SCRIPT: Converting OTF font to WOFF, WOFF2"
@@ -27,8 +31,16 @@ else
   exit 1
 fi
 
+FILENAME=$(basename "${SOURCE%.otf}")
+FAMILY=`basename -s .otf $SOURCE | sed -e "s/-.*//"`
 mkdir -p $DESTINATION/$FAMILY
 
+if type fontforge >& /dev/null; then
+  echo -e "$SCRIPT: fontforge found (OK)"
+else
+  echo -e "$SCRIPT: installing fontforge..."
+  sudo apt-get install -y fontforge
+fi
 if type mkeot >& /dev/null; then
   echo -e "$SCRIPT: mkeot found (OK)"
 else
@@ -53,7 +65,7 @@ mkeot ${SOURCE} > ${EOT_PATH}
 echo -e "$SCRIPT: `ls -s ${EOT_PATH}`"
 
 TTF_PATH="$DESTINATION/$FAMILY/$FILENAME.ttf"
-eot2ttf ${EOT_PATH} ${TTF_PATH}
+fontforge -lang=ff -c 'Open($1); Generate($2); Close();' ${SOURCE} ${TTF_PATH}
 echo -e "$SCRIPT: `ls -s ${TTF_PATH}`"
 
 WOFF_PATH="$DESTINATION/$FAMILY/$FILENAME.woff"
@@ -62,7 +74,7 @@ mv "`dirname $SOURCE`/$FILENAME.woff" ${WOFF_PATH}
 echo -e "$SCRIPT: `ls -s ${WOFF_PATH}`"
 
 WOFF2_PATH="$DESTINATION/$FAMILY/$FILENAME.woff2"
-woff2_compress ${TTF_PATH} > ${WOFF2_PATH} 
+woff2_compress ${TTF_PATH} 
 echo -e "$SCRIPT: `ls -s ${WOFF2_PATH}`"
 
 echo -e "$SCRIPT: END `date`"
--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -20,6 +20,40 @@
         <v-list class="mx-auto" max-width="600">
           <v-list-item>
             <v-list-item-title>
+              {{$t('ebt.audio')}}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              <v-container>
+                <v-row dense>
+                  <v-col >
+                    <v-select id="ips-select" 
+                      ref="sound-focus"
+                      @update:menu="onAudioUpdated"
+                      :items="ipsItems"
+                      class="ebt-select caption"
+                      v-model="settings.ips"
+                      >
+                    </v-select>
+                    {{bellAudio}}
+                    <template v-for="bell,i in ipsChoices">
+                      {{JSON.stringify({i, bell})}}
+                      <audio v-if="bell.value" 
+                        :ref="el => {bellAudio[i] = el}" preload=auto>
+                        <source type="audio/mp3" :src="bell.url.substring(1)" />
+                        <p>{{$t('ebt.noHTML5')}}</p>
+                      </audio>
+                      <v-btn @click="playBell(i)">
+                        {{bell.value}} 
+                        {{$t(`ebt.${bell.i18n}`)}}
+                      </v-btn>
+                    </template>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-list-item-subtitle>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>
               {{$t('ebt.server')}}
             </v-list-item-title>
             <v-list-item-subtitle>
@@ -83,8 +117,11 @@
 <script setup>
 import { ref, reactive, onMounted, computed, } from 'vue';
 import { useSettingsStore } from "../stores/settings";
+import { default as EbtSettings } from "../ebt-settings.mjs";
 import Version from "./Version.vue";
 
+const bellAudio = ref({});
+const ipsChoices = EbtSettings.IPS_CHOICES;
 const dialog = ref(false);
 const settings = useSettingsStore();
 const host = ref(undefined);
@@ -108,16 +145,37 @@ function resetDefaults() {
   console.log("Settings.resetDefaults()", settings);
 }
 
+function playBell(ips=settings.ips) {
+  let ipsChoice = ipsChoices[ips];
+  let audio = bellAudio.value[ips.value];
+  if (audio) {
+    let msg = `playBell(${ips}:${ipsChoice.i18n}) => ${ipsChoice.url}`;
+    audio.play()
+      .then(res=>console.log(msg))
+      .catch(e=>console.log(e));
+  } else {
+    console.warn(`playBell(${ips}) NO AUDIO:`, ipsChoice);
+  }
+}
+
+function onAudioUpdated(open) {
+  console.log(`onAudioUpdate`, open, settings.ips);
+  !open && playBell();
+}
+
 onMounted(()=>{
   host.value = window.location.host;
   console.log('Settings.mounted() settings:', settings);
 });
+
 
 </script>
 <script>
 export default {
   data: function() {
     return {};
+  },
+  methods: {
   },
   computed: {
     themes: (ctx)=>{
@@ -131,6 +189,10 @@ export default {
       }];
       return result;
     },
+    ipsItems: (ctx) => ctx.ipsChoices.map(ic=>({
+      value: ic.value,
+      title: ctx.$t(`ebt.${ic.i18n}`),
+    })),
   },
 }
 </script>
