1157949b7129fe2ea78b01e0a79bf58dac4cdadd
Karl Lew <karl@firepick.org>
2022-09-15 06:21:32 -0700

    v1.9.0 += languages.mjs

---
+++ scripts/i18n-langs.cjs
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require( "path");
+const tsImport = require("ts-import");
+
+const I18NDIR = path.join(__dirname, '../src/i18n');
+
+let args = process.argv;
+let [nodePath, progPath, key, value] = args;
+let script = path.basename(progPath);
+let voices = require('../src/voices.json')
+  .reduce((a,v) => {
+    a[v.langTrans] = true;
+    return a;
+  }, {});;
+
+(async ()=>{
+  let files = await fs.promises.readdir(I18NDIR);
+  let langs = [];
+  for (f of files) {
+    let fpath = path.join(I18NDIR, f);
+    let lang = f.replace(/\..*/,'');
+    let json = await tsImport.load(fpath)
+    let languageCode = json.default?.ebt?.languageCode;
+    langs.push({
+      value: lang,
+      title: languageCode,
+      voice: !!voices[lang],
+    });
+    langs.sort((a,b)=> a.value.localeCompare(b.value));
+  }
+  let voiceLangs = langs.filter(lang=>lang.voice);
+  console.log(`
+// GENERATED CODE (DO NOT EDIT)
+const VOICE_LANGS = ${JSON.stringify(voiceLangs, null, 2)};
+const UI_LANGS = ${JSON.stringify(langs, null, 2)};
+
+export default class Languages {
+  static get VOICE_LANGS() { return VOICE_LANGS };
+  static get UI_LANGUAGES() { return UI_LANGS };
+}
+`);
+
+})()
+
---
+++ src/languages.mjs
@@ -0,0 +1,107 @@
+
+// GENERATED CODE (DO NOT EDIT)
+const VOICE_LANGS = [
+  {
+    "value": "de",
+    "title": "Deutsch / DE",
+    "voice": true
+  },
+  {
+    "value": "en",
+    "title": "English / EN",
+    "voice": true
+  },
+  {
+    "value": "ja",
+    "title": "日本語 / JA",
+    "voice": true
+  },
+  {
+    "value": "pt",
+    "title": "Português / PT",
+    "voice": true
+  }
+];
+const UI_LANGS = [
+  {
+    "value": "cs",
+    "title": "Čeština / CS",
+    "voice": false
+  },
+  {
+    "value": "da",
+    "title": "Dansk / DA",
+    "voice": false
+  },
+  {
+    "value": "de",
+    "title": "Deutsch / DE",
+    "voice": true
+  },
+  {
+    "value": "en",
+    "title": "English / EN",
+    "voice": true
+  },
+  {
+    "value": "fr",
+    "title": "Français / FR",
+    "voice": false
+  },
+  {
+    "value": "hi",
+    "title": "हिंदी / HI",
+    "voice": false
+  },
+  {
+    "value": "is",
+    "title": "Íslenska / IS",
+    "voice": false
+  },
+  {
+    "value": "ja",
+    "title": "日本語 / JA",
+    "voice": true
+  },
+  {
+    "value": "nb",
+    "title": "Norsk / NB",
+    "voice": false
+  },
+  {
+    "value": "nl",
+    "title": "Nederlands / NL",
+    "voice": false
+  },
+  {
+    "value": "pl",
+    "title": "Polski / PL",
+    "voice": false
+  },
+  {
+    "value": "pt",
+    "title": "Português / PT",
+    "voice": true
+  },
+  {
+    "value": "ro",
+    "title": "Română / RO",
+    "voice": false
+  },
+  {
+    "value": "si",
+    "title": "සිංහල / SI",
+    "voice": false
+  },
+  {
+    "value": "vi",
+    "title": "Tiếng Việt / VI",
+    "voice": false
+  }
+];
+
+export default class Languages {
+  static get VOICE_LANGS() { return VOICE_LANGS };
+  static get UI_LANGUAGES() { return UI_LANGS };
+}
+
