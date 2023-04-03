e4726853c6e1a4008aa334e1faaafc0272a18bf0
Karl Lew <karl@firepick.org>
2023-04-03 08:17:12 -0700

    v1.165.0 += index

--- src/index.mjs
+++ src/index.mjs
@@ -6,13 +6,32 @@ import {
 } from 'scv-esm/main.mjs';
 
 import { default as BilaraWeb } from "./bilara-web.mjs";
-import { default as Settings } from "./ebt-settings.mjs";
+import { default as EbtCard } from "./ebt-card.mjs"
+import { default as EbtSettings } from "./ebt-settings.mjs";
+import { default as IdbAudio } from "./idb-audio.mjs"
+import { default as IdbSutta } from "./idb-sutta.mjs"
+import { default as Languages } from "./languages.mjs"
+import { default as SuttaDuration } from "./sutta-duration.mjs"
+import { default as Utils } from "./utils.mjs"
+import { default as Voices } from "./auto/voices.mjs"
+import { default as stores } from "./stores/index.mjs"
 
 export {
-  BilaraWeb,
+  stores,
+
   BilaraPath,
-  Settings,
+  BilaraWeb,
+  EbtCard,
+  EbtSettings,
+  IdbAudio,
+  IdbSutta,
+  Languages,
   SuttaCentralId,
+  SuttaDuration,
   SuttaRef,
   Tipitaka,
+  Utils,
+  Voices,
+
 }
+
---
+++ src/stores/index.mjs
@@ -0,0 +1,13 @@
+
+import { default as AudioStore } from "./audio.mjs"
+import { default as SettingsStore } from "./settings.mjs"
+import { default as SuttasStore } from "./suttas.mjs"
+import { default as VolatileStore } from "./volatile.mjs"
+
+export {
+  AudioStore,
+  SettingsStore,
+  SuttasStore,
+  VolatileStore,
+}
+
