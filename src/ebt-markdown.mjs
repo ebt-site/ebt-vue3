2d0c378923afd3654904bd7f077a754ecf700150
Karl Lew <karl@firepick.org>
2023-05-05 15:58:58 -0700

    v1.207.0 += ebt-markdown

---
+++ src/ebt-markdown.mjs
@@ -0,0 +1,88 @@
+import { default as MarkdownIt } from 'markdown-it';
+
+
+export default class EbtMarkdown {
+  constructor(markdown='', opts={}) {
+    let lines = markdown.split('\n');
+
+    let { 
+      basePath='basePath-undefined',
+      wikiPath="wikiPath-undefined",
+    } = opts;
+    Object.assign(this, { basePath, wikiPath });
+
+    if (lines[0] === '---') {
+      this.heading = {};
+      let iLine;
+      for (iLine = 1; iLine < lines.length; iLine++) {
+        let line = lines[iLine];
+        if (line === '---') {
+          lines = lines.splice(iLine+1);
+          break;
+        }
+        let [key, ...valueParts] = line.split(/:[ \t]*/);
+        let value = valueParts.join(': ');
+        switch (key) {
+          case 'img':
+          case 'img-alt':
+          case 'title': 
+          case 'description':
+            this.heading[key] = value;
+            break;
+          default: 
+            this[key] = value.join(': ');
+            break;
+        }
+      }
+    }
+
+    var mdit = new MarkdownIt();
+    this.htmlLines = mdit.render(lines.join('\n')).trim().split('\n');
+    let htmlHeading = this.htmlHeading();
+    if (htmlHeading) {
+      this.htmlLines = [...htmlHeading, ...this.htmlLines];
+    }
+  }
+
+  htmlHeading() {
+    let { basePath, wikiPath, heading, } = this;
+    if (heading == null) {
+      return '';
+    }
+    let pathParts = wikiPath.split('/');
+    if (pathParts[0] == '') {
+      pathParts = pathParts.slice(1);
+    }
+    let head = ['<div class="ebt-wiki-heading">'];
+    let tail = ['</div><!--ebt-wiki-heading-->'];
+    let spaces = '';
+    let { 
+      description,
+      title, 
+      img, 
+      'img-alt':imgAlt,
+    } = heading;
+    if (img) {
+      head.push('  <a target="_blank">');
+      head.push(`    <img src="img/${img}" />`);
+      head.push('  </a>');
+    }
+    head.push('  <div>');
+    head.push('    <div class="text-caption">');
+    let iLast = pathParts.length-1;
+    for (let i = 0; i < iLast; i++) {
+      let part = pathParts[i];
+      let href = basePath + pathParts.slice(0, i+1).join('/');
+      head.push(`      <a href="${href}" class="nuxt-link-active">`);
+      head.push(`        ${part}`);
+      head.push(`      </a>&nbsp;&gt;&nbsp;`);
+    }
+    head.push(`      ${pathParts[iLast]}`);
+    head.push('    </div>');
+    head.push('  </div>');
+    return [...head, ...tail];
+  }
+
+}
+
+
---
+++ test/ebt-markdown.mjs
@@ -0,0 +1,75 @@
+import { default as EbtMarkdown } from '../src/ebt-markdown.mjs'
+import { logger } from 'log-instance';
+import should from "should";
+
+logger.logLevel = 'warn';
+
+(typeof describe === 'function') && describe("ebt-markdown.mjs", function () {
+  it("TESTTESTdefault ctor", ()=>{
+    let emd = new EbtMarkdown();
+    should(emd).properties({htmlLines:['']});
+    should.deepEqual(Object.keys(emd).sort(), [
+      'htmlLines', 
+      'wikiPath',
+      'basePath',
+    ].sort());
+    should(emd).properties({
+      wikiPath: 'wikiPath-undefined',
+    });
+  });
+  it("TESTTESTheading", ()=>{
+    let emd = new EbtMarkdown(`### Title\ntext`);
+    should.deepEqual(emd.htmlLines, [
+      '<h3>Title</h3>',
+      '<p>text</p>',
+    ]);
+  });
+  it("TESTTESTlink", ()=>{
+    let emd = new EbtMarkdown('a [link](https://x/y) b');
+    should.deepEqual(emd.htmlLines, [
+      '<p>a <a href="https://x/y">link</a> b</p>',
+    ]);
+  });
+  it("TESTTESTheading", ()=>{
+    let md = [
+      '---',
+      'title: test-title',
+      'img: test-img',
+      'img-alt: test-img-alt',
+      '---',
+      'test-body',
+      '',
+    ].join('\n');
+    let basePath = '/test-basePath/';
+    let wikiBase = 'test-wikiPath';
+    let wikiPath = `${wikiBase}/a/b`;
+    let emd = new EbtMarkdown(md, {basePath, wikiPath});
+    should(emd).properties({
+      heading: {
+        title: 'test-title',
+        img: 'test-img',
+        'img-alt': 'test-img-alt',
+      },
+    });
+    let delimiter = '&nbsp;&gt;&nbsp;';
+    should.deepEqual(emd.htmlLines, [
+      '<div class="ebt-wiki-heading">',
+      '  <a target="_blank">',
+      '    <img src="img/test-img" />',
+      '  </a>',
+      '  <div>',
+      '    <div class="text-caption">',
+      `      <a href="${basePath}${wikiBase}" class="nuxt-link-active">`,
+      '        test-wikiPath',
+      `      </a>${delimiter}`,
+      `      <a href="${basePath}${wikiBase}/a" class="nuxt-link-active">`,
+      '        a',
+      `      </a>${delimiter}`,
+      '      b',
+      '    </div>',
+      '  </div>',
+      '</div><!--ebt-wiki-heading-->',
+      '<p>test-body</p>',
+    ]);
+  });
+});
