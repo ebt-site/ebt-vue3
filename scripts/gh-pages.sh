facff1d62d48a69ec234720a7e11e63b529e1ad2
Karl Lew <karl@firepick.org>
2023-05-29 10:00:05 -0700

    PUBLISH-NPM: v

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -6,20 +6,6 @@ APP=$DIR/..
 
 git status | grep "up to date"; RC=$?
 
-#set -e # Fail on error
-#
-#if [ "$RC" != "0" ]; then
-#  echo "$SCRIPT: ERROR: local changes have not been pushed"
-#  exit 1
-#fi
-
-#echo "$SCRIPT: pushing to gh-pages"
-#git subtree push --prefix dist origin gh-pages
-#git checkout gh-pages
-#git pull
-#git checkout main
-
-
 SCRIPT=`basename $0`
 echo -e "$SCRIPT: BEGIN"
 
