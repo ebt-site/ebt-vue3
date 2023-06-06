cad41073fb350c2d5b32909a5de7bbf57ff9efac
Karl Lew <karl@firepick.org>
2023-06-06 06:20:51 -0700

    v1.228.0 += EbtMain

---
+++ src/ebt-main.mjs
@@ -0,0 +1,66 @@
+import { createApp, ref } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+import { logger } from 'log-instance';
+logger.logLevel = 'warn';
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import './assets/main.css'
+import 'vuetify/styles'
+
+import { default as VuetifyOpts } from "./vuetify-opts.mjs";
+import { createI18n, useI18n } from 'vue-i18n';
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n';
+import { default as messages } from './auto/messages.mjs';
+import { createVuetify, } from "vuetify"
+import { loadFonts } from './plugins/webfontloader'
+import NotFound from './components/NotFound.vue';
+import EbtCards from './components/EbtCards.vue';
+import * as VueRouter from "vue-router";
+import { default as EbtConfig } from "../ebt-config.mjs";
+
+export default class EbtMain {
+  static async main(opts={}) {
+    let {
+      config=EbtConfig,
+    } = opts;
+    const msg = "EbtMain.main() ";
+    const pinia = createPinia();
+
+    const vuetifyOpts = VuetifyOpts.options();
+
+    const i18n = new createI18n({
+      legacy: false,
+      locale: "en",
+      fallbackLocale: "en",
+      messages,
+    });
+    vuetifyOpts.adapter = createVueI18nAdapter({ i18n, useI18n });
+
+    // Vuetify
+    const vuetify = createVuetify(vuetifyOpts);
+
+    loadFonts()
+
+    const routes = [
+      { path: '/', component:EbtCards },
+      { path: '/:context', component:EbtCards },
+      { path: '/:context/:location(.*)*', component:EbtCards },
+      { path: '/:bad_path(.*)*', component:NotFound },
+    ]
+    const router = VueRouter.createRouter({
+      history: VueRouter.createWebHashHistory(),
+      routes, 
+    })
+
+    var app = createApp(App);
+    app.provide('config', config);
+    console.log(msg, config);
+    app.use(i18n);
+    app.use(pinia);
+    app.use(vuetify);
+    app.use(router);
+    app.mount('#app');
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,61 +1,6 @@
-import { createApp, ref } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-import { logger } from 'log-instance';
-logger.logLevel = 'warn';
-
-// Styles
-import '@mdi/font/css/materialdesignicons.css'
-import './assets/main.css'
-import 'vuetify/styles'
-
-import { default as VuetifyOpts } from "./vuetify-opts.mjs";
-import { createI18n, useI18n } from 'vue-i18n';
-import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n';
-import { default as messages } from './auto/messages.mjs';
-import { createVuetify, } from "vuetify"
-import { loadFonts } from './plugins/webfontloader'
-import NotFound from './components/NotFound.vue';
-import EbtCards from './components/EbtCards.vue';
-import * as VueRouter from "vue-router";
+import { default as EbtMain } from './ebt-main.mjs';
 import { default as EbtConfig } from "../ebt-config.mjs";
 
-(async() => {
-  const msg = "main.js ";
-  const pinia = createPinia();
-
-  const vuetifyOpts = VuetifyOpts.options();
-
-  const i18n = new createI18n({
-    legacy: false,
-    locale: "en",
-    fallbackLocale: "en",
-    messages,
-  });
-  vuetifyOpts.adapter = createVueI18nAdapter({ i18n, useI18n });
-
-  // Vuetify
-  const vuetify = createVuetify(vuetifyOpts);
-
-  loadFonts()
-
-  const routes = [
-    { path: '/', component:EbtCards },
-    { path: '/:context', component:EbtCards },
-    { path: '/:context/:location(.*)*', component:EbtCards },
-    { path: '/:bad_path(.*)*', component:NotFound },
-  ]
-  const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
-    routes, 
-  })
-
-  var app = createApp(App);
-  app.provide('config', EbtConfig);
-  console.log(msg, EbtConfig);
-  app.use(i18n);
-  app.use(pinia);
-  app.use(vuetify);
-  app.use(router);
-  app.mount('#app');
-})()
+EbtMain.main({
+  config: EbtConfig,
+});
