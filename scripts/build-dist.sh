c3545f1acd8f8bc4f1fb6f9d0781e9e74e3fe6de
Karl Lew <karl@firepick.org>
2023-06-15 07:10:05 -0700

    v1.237.0 += build-dist

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -4,20 +4,21 @@ SCRIPT=`basename $0 | tr abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ`
 set -e
 
 if [ ! -e 'package.json' ]; then
-  echo -e SCRIPT: this script must be run from application folder
+  echo -e "SCRIPT: this script must be run from application folder"
   exit 911
 fi
 
 echo -e $SCRIPT: BEGIN `date`
 
-VERSION=`node $DIR/version.cjs`
+VERSION=`node scripts/version.cjs`
+echo -e "$SCRIPT: version $VERSION"
 
 echo "$SCRIPT: vite build"
 vite build
 
 echo $VERSION > dist/version
 cp package.json dist
-cp -r src/i18n dist/assets/
+cp -r $DIR/../src/i18n dist/assets/
 
 echo -e $SCRIPT: END `date`
 
