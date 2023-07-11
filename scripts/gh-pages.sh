89c7f1571bcfffc4447d6d9c2ccb583e117ee55e
Karl Lew <karl@firepick.org>
2023-07-10 19:12:59 -0700

    v1.255.0 += gh-pages

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -36,13 +36,13 @@ rm -rf assets audio fonts img wiki
 echo -e "$SCRIPT: copying new content"
 cp -r dist/* .
 ls -l
-echo "$SCRIPT: adding any new content"
-git add .
 
 git status | grep 'nothing to commit' > /dev/null; RC=$?
 if [ "$RC" == "0" ]; then
   echo "$SCRIPT: Nothing to commit"
 else
+  echo "$SCRIPT: adding any new content"
+  git add .
   echo -e "$SCRIPT: updating github"
   git commit -m "gh-pages"
   git push
