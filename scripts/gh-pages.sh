e839f635a2e1c94517facabba5f1472a48a98bf4
Karl Lew <karl@firepick.org>
2023-07-10 20:22:37 -0700

    v1.258.0 += gh-pages

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -12,7 +12,7 @@ fi
 
 git checkout main
 git status | grep "up to date"; RC=$?
-set -e # exit on error
+#set -e # exit on error
 if [ "$RC" != "0" ]; then
   echo "$SCRIPT: ERROR: local changes have not been pushed"
   exit 1
